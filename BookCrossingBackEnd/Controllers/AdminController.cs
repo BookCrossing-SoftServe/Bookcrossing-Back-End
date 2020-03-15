f2b25fa9d3129489a8bbb923c651d7b76a4de377
romadmn <ferencrman@gmail.com>
2020-03-15 17:19:35 +0200

    Add admin controller

--- BookCrossingBackEnd/Controllers/AdminController.cs
+++ BookCrossingBackEnd/Controllers/AdminController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.IServices;
+using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,6 +13,55 @@ namespace BookCrossingBackEnd.Controllers
     [Route("api/[controller]")]
     public class AdminController : Controller
     {
-       
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+        [HttpGet]
+        public List<User> GetListOfUsers()
+        {
+            return _adminService.ListAllUsers().ToList();
+        }
+        [HttpGet]
+        public List<Genre> GetListOfGenres()
+        {
+            return _adminService.ListAllGenres().ToList();
+        }
+        [HttpPost]
+        public IActionResult AddNewGenre(Genre genre)
+        {
+            _adminService.AddNewGenre(genre);
+            return Ok(genre);
+        }
+        [HttpDelete]
+        public IActionResult RemoveGenre(int genreId)
+        {
+            try
+            {
+                _adminService.RemoveGenre(genreId);
+                return Ok("Successfully deleted");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw new Exception("Bad gerne id!");
+            }
+        }
+        [HttpDelete]
+        public IActionResult RemoveUser(int userId)
+        {
+            try
+            {
+                _adminService.RemoveUser(userId);
+                return Ok("Successfully deleted");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw new Exception("Bad user id!");
+            }
+        }
     }
 }
