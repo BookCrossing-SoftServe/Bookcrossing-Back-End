753d82b7cce3d20b01fa1763e4d043c7804325cc
Maryna Diuh <maradug13@gmail.com>
2020-03-21 02:39:17 +0300

    Closes #53 Add Mapper

--- src/Application/Dto/RoomLocationDto.cs
+++ src/Application/Dto/RoomLocationDto.cs
@@ -6,8 +6,8 @@ using Domain.Entities;
 namespace Application.Dto
 {
     public class RoomLocationDto
-    { 
-        public LocationDto Location { get; set; }
+    {
+       public LocationDto Location { get; set; }
        public int RoomNumber { get; set; }
     }
 }
