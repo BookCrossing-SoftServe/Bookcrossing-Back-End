5a868fd097b03567ac0d2840a1540209330007dc
Maksym <mm.novitskyi@gmail.com>
2020-03-20 23:51:59 +0200

    XML Comments for Services.IAuthor

--- src/Application/Services/Interfaces/IAuthor.cs
+++ src/Application/Services/Interfaces/IAuthor.cs
@@ -9,10 +9,38 @@ namespace Application.Services.Interfaces
 {
     public interface IAuthor
     {
+        /// <summary>
+        /// Retrieve Author by ID
+        /// </summary>
+        /// <param name="authorId">Author's ID</param>
+        /// <returns></returns>
         Task<Author> GetById(int authorId);
+
+        /// <summary>
+        /// Retrieve all Authors
+        /// </summary>
+        /// <returns></returns>
         Task<List<Author>> GetAll();
+
+        /// <summary>
+        /// Update specified Author
+        /// </summary>
+        /// <param name="author">Author's DTO instance</param>
+        /// <returns></returns>
         Task Update(AuthorDto author);
+
+        /// <summary>
+        /// Remove specified Author
+        /// </summary>
+        /// <param name="author">Author's DTO instance</param>
+        /// <returns></returns>
         Task Remove(Author author);
+
+        /// <summary>
+        /// Create new author and add it into DbContext
+        /// </summary>
+        /// <param name="author">Author's DTO instance</param>
+        /// <returns></returns>
         Task<Author> Add(AuthorDto author);
     }
 }
