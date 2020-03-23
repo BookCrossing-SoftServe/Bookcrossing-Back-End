4a6fc84aeefd23746f2797ac87729f7caede9e7a
romadmn <ferencrman@gmail.com>
2020-03-23 19:15:34 +0200

    Add request validator

---
+++ src/BookCrossingBackEnd/Validators/RequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Dto;
+using FluentValidation;
+
+namespace BookCrossingBackEnd.Validators
+{
+    public class RequestValidator : AbstractValidator<RequestDto>
+    {
+        public RequestValidator()
+        {
+            RuleFor(x => x.Id).GreaterThan(0);
+            RuleFor(x => x.BookId).GreaterThan(0);
+            RuleFor(x => x.OwnerId).GreaterThan(0);
+            RuleFor(x => x.RequestDate).NotNull();
+        }
+    }
+}
