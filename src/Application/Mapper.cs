1933efbe6bb9c35cc482f1a022ad692822230d84
Mykhailo Ivanovych <ivanovych.mykhailo@gmail.com>
2020-03-24 19:52:41 +0200

    Changed Automapper config

--- src/Application/Mapper.cs
+++ src/Application/Mapper.cs
@@ -13,9 +13,14 @@ namespace Application
         public Mapper()
         {
             CreateMap<AuthorDto, Author>().ReverseMap().ForMember(a => a.Id, opt => opt.Condition(a => a.Id != 0));
-            CreateMap<BookDto, Book>().ReverseMap()
+            CreateMap<AuthorDto, BookAuthor>().ReverseMap();
+            CreateMap<GenreDto, BookGenre>().ReverseMap();
+            //CreateMap<BookDto, Book>()
+            //    .ForMember(entity => entity.BookAuthor.Select(b => b.Author), opt => opt.MapFrom(x => x.Authors))
+            //    .ForMember(entity => entity.BookGenre.Select(b => b.Genre), opt => opt.MapFrom(x => x.Genres));
+            CreateMap<Book, BookDto>()
                 .ForMember(dto => dto.Authors, opt => opt.MapFrom(x => x.BookAuthor.Select(y => y.Author).ToList()))
-                .ForMember(dto => dto.Genres, opt => opt.MapFrom(x => x.BookGenre.Select(y => y.Genre).ToList()));
+                .ForMember(dto => dto.Genres, opt => opt.MapFrom(x => x.BookGenre.Select(y => y.Genre).ToList())).ReverseMap();
             CreateMap<GenreDto, Genre>().ReverseMap();
             CreateMap<LocationDto, Location>().ReverseMap();
             CreateMap<RoomLocationDto, UserLocation>().ReverseMap();
