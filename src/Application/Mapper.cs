6403c289440aa2bbd4cebd32cada9e66d76d94c6
Mykhailo Ivanovych <ivanovych.mykhailo@gmail.com>
2020-03-25 19:44:09 +0200

    Added mapping config for Book Fixed post method of Book Controller

--- src/Application/Mapper.cs
+++ src/Application/Mapper.cs
@@ -13,14 +13,37 @@ namespace Application
         public Mapper()
         {
             CreateMap<AuthorDto, Author>().ReverseMap().ForMember(a => a.Id, opt => opt.Condition(a => a.Id != 0));
-            CreateMap<AuthorDto, BookAuthor>().ReverseMap();
-            CreateMap<GenreDto, BookGenre>().ReverseMap();
-            //CreateMap<BookDto, Book>()
-            //    .ForMember(entity => entity.BookAuthor.Select(b => b.Author), opt => opt.MapFrom(x => x.Authors))
-            //    .ForMember(entity => entity.BookGenre.Select(b => b.Genre), opt => opt.MapFrom(x => x.Genres));
+            CreateMap<AuthorDto, BookAuthor>()
+                .ForMember(a => a.AuthorId, opt => opt.MapFrom(dto => dto.Id))
+                .ForMember(a => a.Author, opt =>
+                {
+                    opt.Condition(dto => dto.Id == 0);
+                    opt.MapFrom(dto => dto);
+                });
+            CreateMap<GenreDto, BookGenre>()
+                .ForMember(a => a.GenreId, opt => opt.MapFrom(dto => dto.Id))
+                .ForMember(a => a.Genre, opt =>
+                {
+                    opt.Condition(dto => dto.Id == 0);
+                    opt.MapFrom(dto => dto);
+                });
+            CreateMap<BookDto, Book>()
+                .ForMember(entity => entity.BookAuthor, opt => opt.MapFrom(x => x.Authors))
+                .ForMember(entity => entity.BookGenre, opt => opt.MapFrom(x => x.Genres))
+                .AfterMap((model, entity) =>
+                    {
+                        foreach (var item in entity.BookAuthor)
+                        {
+                            item.Book = entity;
+                        }
+                        foreach (var item in entity.BookGenre)
+                        {
+                            item.Book = entity;
+                        }
+                    }); 
             CreateMap<Book, BookDto>()
                 .ForMember(dto => dto.Authors, opt => opt.MapFrom(x => x.BookAuthor.Select(y => y.Author).ToList()))
-                .ForMember(dto => dto.Genres, opt => opt.MapFrom(x => x.BookGenre.Select(y => y.Genre).ToList())).ReverseMap();
+                .ForMember(dto => dto.Genres, opt => opt.MapFrom(x => x.BookGenre.Select(y => y.Genre).ToList()));
             CreateMap<GenreDto, Genre>().ReverseMap();
             CreateMap<LocationDto, Location>().ReverseMap();
             CreateMap<RoomLocationDto, UserLocation>().ReverseMap();
