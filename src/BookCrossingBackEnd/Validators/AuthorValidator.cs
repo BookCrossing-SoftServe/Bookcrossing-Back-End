238babfdd21f55664462a55b988b2402e6634940
Maksym <mm.novitskyi@gmail.com>
2020-03-25 17:06:52 +0200

    Fixed mistake in AuthorValidator

--- src/BookCrossingBackEnd/Validators/AuthorValidator.cs
+++ src/BookCrossingBackEnd/Validators/AuthorValidator.cs
@@ -8,8 +8,8 @@ namespace BookCrossingBackEnd.Validators
         public AuthorValidator()
         {
             RuleFor(x => x.Id).GreaterThanOrEqualTo(0);
-            RuleFor(x => x.FirstName).Length(2, 30);
-            RuleFor(x => x.LastName).Length(2, 30);
+            RuleFor(x => x.FirstName).Length(2, 20);
+            RuleFor(x => x.LastName).Length(2, 20);
             RuleFor(x => x.MiddleName).Length(0, 30);
         }
     }
