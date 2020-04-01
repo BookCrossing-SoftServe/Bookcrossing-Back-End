aeff5a76c4d32ded97c706d0cb6bec6b03d1704e
Maksym <mm.novitskyi@gmail.com>
2020-04-01 13:39:30 +0300

    Closes 093 Implement IDisosible For Base Repository

--- src/Infastructure/BaseRepository.cs
+++ src/Infastructure/BaseRepository.cs
@@ -2,28 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
 using Domain.Entities;
 using Domain;
-using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure
 {
-    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class, IEntityBase
+    public class BaseRepository<TEntity> : IDisposable, IRepository<TEntity> where TEntity : class, IEntityBase
     {
         protected readonly BookCrossingContext Context;
-        protected readonly DbSet<TEntity> Entities;
+        protected DbSet<TEntity> Entities;
 
         public BaseRepository(BookCrossingContext context)
         {
             Context = context;
             Entities = context.Set<TEntity>();
         }
-        public virtual async Task<List<TEntity>> GetAllAsync()
+        public virtual IQueryable<TEntity> GetAll()
         {
-            return await Entities.ToListAsync();
+            return Entities.AsQueryable();
         }
         public virtual async Task<TEntity> FindByIdAsync(params object[] keys)
         {
@@ -34,15 +32,11 @@ namespace Infrastructure
 
             return await Entities.FirstOrDefaultAsync(predicate);
         }
-        public virtual IQueryable<TEntity> GetAll()
-        {
-            return Entities.AsQueryable();
-        }
         public virtual void Add(TEntity entity)
         {
             Entities.Add(entity);
         }
-        public virtual void AddRange(IEnumerable<TEntity> entity)
+        public void AddRange(IEnumerable<TEntity> entity)
         {
             Entities.AddRange(entity);
         }
@@ -50,7 +44,7 @@ namespace Infrastructure
         {
             Entities.Remove(entity);
         }
-        public virtual void RemoveRange(IEnumerable<TEntity> entity)
+        public void RemoveRange(IEnumerable<TEntity> entity)
         {
             Entities.RemoveRange(entity);
         }
@@ -58,10 +52,38 @@ namespace Infrastructure
         {
             Entities.Update(entity);
         }
-
         public async Task SaveChangesAsync()
         {
             await Context.SaveChangesAsync();
         }
+
+        #region IDisposable Support
+        private bool _disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    Entities = null;
+                }
+                Context?.Dispose();
+                _disposedValue = true;
+            }
+        }
+        ~BaseRepository()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+
     }
 }
