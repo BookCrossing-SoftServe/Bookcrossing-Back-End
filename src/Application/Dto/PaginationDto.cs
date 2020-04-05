182356d19cd5c53932db094dd3c7ace543c50856
Maksym <mm.novitskyi@gmail.com>
2020-04-05 18:36:38 +0300

    Serachless Version

---
+++ src/Application/Dto/PaginationDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities;
+
+namespace Application.Dto
+{
+    public class PaginationDto<T> where T : class
+    {
+        public List<T> Page { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
