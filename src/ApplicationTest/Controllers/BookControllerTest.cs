2e8dea4be1b739eba478f2e51f2e985219175fc4
Mykhailo Ivanovych <ivanovych.mykhailo@gmail.com>
2020-04-14 02:10:35 +0300

    Add Get/id and Put unit tests

--- src/ApplicationTest/Controllers/BookControllerTest.cs
+++ src/ApplicationTest/Controllers/BookControllerTest.cs
@@ -12,19 +12,20 @@ using System.Threading.Tasks;
 namespace ApplicationTest.Controllers
 {
     [TestFixture]
-    class BookControllerTest
+    class BooksControllerTest
     {
         private Mock<IBookService> _bookService;
 
-        public BookControllerTest()
+        public BooksControllerTest()
         {
             _bookService = new Mock<IBookService>();
         }
 
         [Test]
-        public async Task TestGetAll()
+        public async Task GetAllBooksTest()
         {
-            _bookService.Setup(s => s.GetAll()).ReturnsAsync((GetTestBooks()));
+            var testBooks = GetTestBooks();
+            _bookService.Setup(s => s.GetAll()).ReturnsAsync(testBooks);
             var booksController = new BooksController(_bookService.Object);
 
             var getAllBooksResult = await booksController.GetAllBooks();
@@ -32,16 +33,44 @@ namespace ApplicationTest.Controllers
             var okResult = getAllBooksResult.Result as OkObjectResult;
             okResult.Should().BeOfType<OkObjectResult>();
             var books = okResult.Value as List<BookDto>;
-            books.Count().Should().Be(GetTestBooks().Count);
+            books.Count().Should().Be(testBooks.Count);
         }
 
         private List<BookDto> GetTestBooks()
         {
-            return  new List<BookDto>
+            return new List<BookDto>
             {
-                new BookDto(),
-                new BookDto()
+                new BookDto(){Id = 1},
+                new BookDto(){Id = 2}
             };
+        }
+
+        [Test]
+        public async Task GetBookTest()
+        {
+            var testBook = GetTestBooks().FirstOrDefault();
+            _bookService.Setup(s => s.GetById(It.IsAny<int>())).ReturnsAsync(testBook);
+            var booksController = new BooksController(_bookService.Object);
+
+            var getBookResult = await booksController.GetBook(1);
+
+            var okResult = getBookResult.Result as OkObjectResult;
+            okResult.Should().BeOfType<OkObjectResult>();
+            var resultBook = okResult.Value as BookDto;
+            resultBook.Id.Should().Be(testBook.Id);
+        }
+
+        [Test]
+        public async Task PutBookTest()
+        {
+            _bookService.Setup(m => m.Update(It.IsAny<BookDto>())).Verifiable();
+            var booksController = new BooksController(_bookService.Object);
+            var testBook = GetTestBooks().FirstOrDefault();
+
+            var putBookResult = await booksController.PutBook(testBook.Id, testBook);
+
+            putBookResult.Should().BeOfType<NoContentResult>();
+            _bookService.Verify(m => m.Update(It.IsAny<BookDto>()), Times.Once);
         }
 
     }
