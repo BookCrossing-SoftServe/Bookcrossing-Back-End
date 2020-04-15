c28e941f801610042fbd9c7c8f64ba2e577791f8
Mykhailo Ivanovych <ivanovych.mykhailo@gmail.com>
2020-04-15 12:50:54 +0300

    Merged

---
+++ src/Application/Services/Interfaces/IUserResolverService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Services.Interfaces
+{
+    public interface IUserResolverService
+    {
+        public int GetUserId();
+        public bool IsUserAdmin();
+    }
+}
