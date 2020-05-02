d129b46df0ff4d7f0f01067ad422b6f9059d73d4
Mykhailo Ivanovych <ivanovych.mykhailo@gmail.com>
2020-05-02 23:07:06 +0300

    Added book dto's

---
+++ src/Application/Dto/BookPutDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Application.Dto
+{
+    public class BookPutDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public string Publisher { get; set; }
+        public bool Available { get; set; }
+        public string Notice { get; set; }
+
+        public List<AuthorDto> Authors { get; set; }
+        public List<GenreDto> Genres { get; set; }
+    }
+}
