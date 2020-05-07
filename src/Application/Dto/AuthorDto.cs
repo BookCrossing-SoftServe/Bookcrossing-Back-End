a76a0cbf21155c8352b3d94b63066dc49f91ffb9
Maryna Diuh <37348506+Digicat13@users.noreply.github.com>
2020-05-07 18:36:21 +0300

    new field added (#199)

--- src/Application/Dto/AuthorDto.cs
+++ src/Application/Dto/AuthorDto.cs
@@ -6,5 +6,6 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string MiddleName { get; set; }
+        public bool IsConfirmed { get; set; }
     }
 }
