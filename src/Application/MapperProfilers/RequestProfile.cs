e0e184dc821bcd39d73537e2a20bfde1090e8a9a
Elite <Elite@DESKTOP-0L03IAF>
2020-05-08 22:31:10 +0200

    Closes #206 - Perfom project refactoring

---
+++ src/Application/MapperProfilers/RequestProfile.cs
@@ -0,0 +1,20 @@
+﻿using Application.Dto;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RdbmsEntities = Domain.RDBMS.Entities;
+namespace Application.MapperProfilers
+{
+    public class RequestProfile : Profile
+    {
+        public RequestProfile()
+        {
+            CreateMap<RequestDto, RdbmsEntities.Request>().ReverseMap()
+               .ForMember(a => a.Id, opt => opt.Condition(a => a.Id != 0))
+               .ForMember(dto => dto.Book, opt => opt.MapFrom(x => x.Book))
+               .ForMember(dto => dto.Owner, opt => opt.MapFrom(x => x.Owner))
+               .ForMember(dto => dto.User, opt => opt.MapFrom(x => x.User));
+        }
+    }
+}
---
+++ src/Application/MapperProfilers/UserProfile.cs
@@ -0,0 +1,33 @@
+﻿using Application.Dto;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RdbmsEntities = Domain.RDBMS.Entities;
+namespace Application.MapperProfilers
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<RdbmsEntities.User, Dto.Comment.OwnerDto>()
+             .ForMember(dto => dto.Role, opt => opt.MapFrom(entity => entity.Role.Name));
+            CreateMap<UserUpdateDto, RdbmsEntities.User>().ReverseMap();
+
+            CreateMap<LoginDto, RdbmsEntities.User>().ReverseMap();
+
+            CreateMap<RegisterDto, RdbmsEntities.User>().ReverseMap();
+
+            CreateMap<RdbmsEntities.User, UserDto>().ReverseMap();
+
+            CreateMap<UserProfileDto, RdbmsEntities.User>().ForMember(x => x.Book, opt => opt.MapFrom(x => x.Books))
+                .ReverseMap();
+
+            CreateMap<UserDto, RdbmsEntities.User>().ReverseMap()
+                .ForMember(a => a.Id, opt => opt.Condition(a => a.Id != 0))
+                .ForMember(dto => dto.UserLocation, opt => opt.MapFrom(x => x.UserLocation))
+                .ForMember(dto => dto.Role, opt => opt.MapFrom(x => x.Role));
+        }
+    }
+
+}
