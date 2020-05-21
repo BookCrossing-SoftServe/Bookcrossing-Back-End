6bd833f59815fcfb4d02f35278658885c59452b0
Maksym <mm.novitskyi@gmail.com>
2020-05-21 11:42:51 +0300

    Increased max length for columns FirstName/LastName in Author Entity

---
+++ src/BookCrossingBackEnd/Migrations/20200521083627_AuthorMaxLengthAdjustments.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookCrossingBackEnd.Migrations
+{
+    public partial class AuthorMaxLengthAdjustments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "lastname",
+                table: "Author",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "firstname",
+                table: "Author",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "lastname",
+                table: "Author",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "firstname",
+                table: "Author",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+    }
+}
