5a7378d1ddca4e2791a51feadf4128b0e8dedbd3
Volodymyr Korpal <volodyakorpal@gmail.com>
2020-05-27 21:37:02 +0300

    added logger to genre controller

--- src/BookCrossingBackEnd/Controllers/GenreController.cs
+++ src/BookCrossingBackEnd/Controllers/GenreController.cs
@@ -5,6 +5,7 @@ using Application.Dto.QueryParams;
 using Application.Services.Interfaces;
 using BookCrossingBackEnd.Filters;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace BookCrossingBackEnd.Controllers
 {
@@ -13,18 +14,22 @@ namespace BookCrossingBackEnd.Controllers
     public class GenreController : ControllerBase
     {
         private readonly IGenreService _genreService;
+        private readonly ILogger _logger;
 
-        public GenreController(IGenreService genreService)
+        public GenreController(IGenreService genreService, ILogger<GenreController> logger)
         {
             _genreService = genreService;
+            _logger = logger;
         }
 
         [HttpGet("{id:min(1)}")]
         public async Task<ActionResult<GenreDto>> GetGenre(int id)
         {
+            _logger.LogInformation("Getting genre {Id}", id);
             var genre = await _genreService.GetById(id);
             if (genre == null)
             {
+                _logger.LogWarning("GetGenre({Id}) NOT FOUND", id);
                 return NotFound();
             }
             return Ok(genre);
@@ -34,6 +39,7 @@ namespace BookCrossingBackEnd.Controllers
         [HttpGet]
         public async Task<ActionResult<List<GenreDto>>> GetAllGenres()
         {
+            _logger.LogInformation("Getting all genres");
             return Ok(await _genreService.GetAll());
         }
 
@@ -42,9 +48,11 @@ namespace BookCrossingBackEnd.Controllers
         [HttpPut]
         public async Task<IActionResult> PutGenre(GenreDto genreDto)
         {
+            _logger.LogInformation("Update genre {Id}", genreDto.Id);
             var updated = await _genreService.Update(genreDto);
             if (!updated)
             {
+                _logger.LogWarning("Update genre ({Id}) NOT FOUND", genreDto.Id);
                 return NotFound();
             }
             return NoContent();
@@ -54,6 +62,7 @@ namespace BookCrossingBackEnd.Controllers
         [HttpPost]
         public async Task<ActionResult<GenreDto>> PostGenre([FromBody]GenreDto genreDto)
         {
+            _logger.LogInformation("Post genre {Id}", genreDto.Id);
             var insertedGenre = await _genreService.Add(genreDto);
             return CreatedAtAction("GetGenre", new { id = insertedGenre.Id }, insertedGenre);
         }
@@ -63,9 +72,11 @@ namespace BookCrossingBackEnd.Controllers
         [HttpDelete("{id:min(1)}")]
         public async Task<IActionResult> DeleteGenre(int id)
         {
+            _logger.LogInformation("Delete genre {Id}", id);
             var genre = await _genreService.Remove(id);
             if (genre == false)
             {
+                _logger.LogWarning("Delete genre ({Id}) NOT FOUND", id);
                 return NotFound();
             }
             return Ok();
@@ -73,6 +84,7 @@ namespace BookCrossingBackEnd.Controllers
         [HttpGet("paginated")]
         public async Task<ActionResult<PaginationDto<GenreDto>>> GetAllGenres([FromQuery] FullPaginationQueryParams fullPaginationQuery)
         {
+            _logger.LogInformation("Getting all paginated genres");
             return Ok(await _genreService.GetAll(fullPaginationQuery));
         }
     }
