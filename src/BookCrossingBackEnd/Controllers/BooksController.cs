6a7e59279790845d68a6977d9284b06fac6f348b
Roman Ferents <ferencrman@gmail.com>
2020-06-03 14:59:18 +0300

    Update BooksController.cs

--- src/BookCrossingBackEnd/Controllers/BooksController.cs
+++ src/BookCrossingBackEnd/Controllers/BooksController.cs
@@ -10,6 +10,7 @@ namespace BookCrossingBackEnd.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class BooksController : ControllerBase
     {
         private readonly IBookService _bookService;
