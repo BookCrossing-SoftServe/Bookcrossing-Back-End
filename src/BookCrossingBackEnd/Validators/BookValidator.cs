7d95a451dd9f488d3eff95bdec9cd78dd693ede0
Святослав Волошко <voloshko.svyatoslav2@gmail.com>
2020-07-02 20:45:08 +0300

    Closes #292 - Make change on 'Register book' page

--- src/BookCrossingBackEnd/Validators/BookValidator.cs
+++ src/BookCrossingBackEnd/Validators/BookValidator.cs
@@ -11,13 +11,11 @@ namespace BookCrossingBackEnd.Validators
             RuleForEach(b => b.Genres).ChildRules(author => author.RuleFor(a => a.Id).GreaterThan(0).NotEmpty());
             RuleForEach(b => b.Authors).ChildRules(genre => genre.RuleFor(a => a.Id).GreaterThan(0).NotEmpty());
             RuleFor(b => b.Name)
-                .Matches(@"^[A-Za-zА-Яа-яі0-9\s\-_,\.;:()?!#]+$")
                 .Length(1, 100)
                 .NotNull();
             RuleFor(b => b.UserId).NotNull();
             RuleFor(b => b.Publisher)
-                .Matches(@"^[A-Za-zА-Яа-яі0-9\s\-_,\.;:()?!]+$")
-                .MaximumLength(100);
+                .Length(1,100);
         }
     }
 }
