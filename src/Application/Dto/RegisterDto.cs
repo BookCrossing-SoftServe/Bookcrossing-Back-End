c4eb1c8af51be2dfae299da1283f99b1b3d8165e
Andriy Oleksyuk <andre200129@gmail.com>
2020-07-06 00:20:07 +0300

    Closes #296 create base registration

--- src/Application/Dto/RegisterDto.cs
+++ src/Application/Dto/RegisterDto.cs
@@ -2,8 +2,8 @@
 {
     public class RegisterDto
     {
+        public int? Id { get; set; }
         public string FirstName { get; set; }
-        public string MiddleName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
