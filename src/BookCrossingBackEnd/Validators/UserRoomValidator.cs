4780ba04ea8b6609804707edb756dcf85daa1bbe
Oles <omelanchuk98@gmail.com>
2020-07-08 18:37:58 +0300

    Added validator for 'Room number'

---
+++ src/BookCrossingBackEnd/Validators/UserRoomValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Dto;
+using FluentValidation;
+
+namespace BookCrossingBackEnd.Validators
+{
+    public class UserRoomValidator: AbstractValidator<RoomLocationDto>
+    {
+        public UserRoomValidator()
+        {
+            RuleFor(x => x.RoomNumber).NotNull().Length(1, 7).Matches(@"^[^\s]{1,7}$");
+        }
+    }
+}
