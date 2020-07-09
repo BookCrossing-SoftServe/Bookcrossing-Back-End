bfc411f94b4d2bc5d895875f57d3818724148cdf
Andriy Oleksiuk <47561246+AndrewOleksiuk@users.noreply.github.com>
2020-07-09 13:16:31 +0300

    Update UsersService.cs

--- src/Application/Services/Implementation/UsersService.cs
+++ src/Application/Services/Implementation/UsersService.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Application.Services.Implementation
