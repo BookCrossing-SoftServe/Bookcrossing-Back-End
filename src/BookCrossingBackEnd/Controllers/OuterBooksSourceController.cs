3e6fd09e05e5b9d710201d0603d738d18b38207f
Oles <omelanchuk98@gmail.com>
2020-07-15 17:34:29 +0300

    Added controller for outersources

---
+++ src/BookCrossingBackEnd/Controllers/OuterBooksSourceController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Dto.QueryParams;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Application.Services.Interfaces;
+
+namespace BookCrossingBackEnd.Controllers
+{
+    [Route("api/outerservice")]
+    [ApiController]
+    public class OuterBooksSourceController : ControllerBase
+    {
+        private readonly IOuterBookSourceService _outerBookSourceService;
+
+        public OuterBooksSourceController(IOuterBookSourceService outerBookSourceService)
+        {
+            _outerBookSourceService = outerBookSourceService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery]OuterSourceQueryParameters query)
+        {
+            var books = await _outerBookSourceService.SearchBooks(query);
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBook(int id)
+        {
+            var book = await _outerBookSourceService.GetBook(id);
+            return Ok(book);
+        }
+    }
+}
