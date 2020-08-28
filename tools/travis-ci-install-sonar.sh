c2472e63b233c2376534ba20faabb30bf0b5d43b
Borys Levkovych <levkovychborya@gmail.com>
2020-08-28 13:14:47 +0300

    maybe will work out?

--- tools/travis-ci-install-sonar.sh
+++ tools/travis-ci-install-sonar.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "Starting install..."
-wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.6.1.2049/sonar-scanner-msbuild-4.6.1.2049-netcoreapp2.0.zip
+wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.10.0.19059/sonar-scanner-msbuild-4.10.0.19059-netcoreapp3.0.zip
 echo "Unzipping..."
 unzip -qq sonar.zip -d tools/sonar
 echo "Displaying file structure..."
 find .
 ls -l tools/sonar
 echo "Changing permissions..."
-chmod +x tools/sonar/sonar-scanner-3.3.0.1492/bin/sonar-scanner
+chmod +x tools/sonar/sonar-scanner-4.4.0.2170/bin/sonar-scanner
